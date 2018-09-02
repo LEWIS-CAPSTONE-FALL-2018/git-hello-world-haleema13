00744dfc5f6100728c0de6a5786d023f0aaf2c88
haleema13 <42900959+haleema13@users.noreply.github.com>
2018-09-02 00:44:10 -0500

    This is my first commit.

---
+++ Assignment1.py
@@ -0,0 +1,9 @@
+#Name: Haleema Mansoor
+#Course: CPSC 49200-02
+#File: Assignment1.py
+#Description: Simple program to display "Hello world!"
+
+def main():
+    print("Hello world!")
+    input()
+main()
